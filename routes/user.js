440c4c8ff59de0c487f5e89fe1941acb3470f0a7
IKun <1228836483@qq.com>
2023-01-12 19:03:28 +0800

    feat: 后台部门管理接口开发

--- routes/user.js
+++ routes/user.js
@@ -5,56 +5,23 @@ const { setResponse } = require('../utils/util')
 router.prefix('/user')
 
 router.get('/list', async ctx => {
-  const { username, userId, startTime, endTime, pageSize, pageNum } =
-    ctx.request.query
+  const { username, userId, startTime, endTime, pageSize, pageNum } = ctx.request.query
   const sql = {
-    $and: [
-      { username: { $regex: username } },
-      {
-        $or: [
-          userId
-            ? {
-                userId: Number(userId)
-              }
-            : {}
-        ]
-      },
-      {
-        $or: [
-          startTime || endTime
-            ? {
-                createTime: {
-                  $gte: new Date(startTime),
-                  $lte: new Date(endTime)
-                }
-              }
-            : {}
-        ]
-      }
-    ]
+    isDelete: 0
   }
-  const res = await User.aggregate([
-    {
-      $match: sql
-    },
-    {
-      $facet: {
-        total: [{ $count: 'count' }],
-        data: [
-          {
-            $skip: (pageNum - 1) * 10
-          },
-          { $limit: Number(pageSize) }
-        ]
-      }
-    },
-    {
-      $project: {
-        data: { rows: '$data', total: { $arrayElemAt: ['$total.count', 0] } }
-      }
-    }
-  ])
-  ctx.body = setResponse(res[0].data[0] || [])
+  if (startTime || endTime) {
+    sql['createTime'] = { $gte: new Date(startTime), $lte: new Date(endTime) }
+  }
+  if (username) {
+    sql['username'] = { $regex: username }
+  }
+  if (userId) {
+    sql['userId'] = { userId: Number(userId) }
+  }
+  const result = await User.find(sql)
+    .skip((pageNum - 1) * 10)
+    .limit(Number(pageSize))
+  ctx.body = setResponse({ rows: result, total: result.length })
 })
 
 router.post('/add', async ctx => {
@@ -63,11 +30,7 @@ router.post('/add', async ctx => {
     $or: [{ username: params.username }, { email: params.email }]
   })
   if (docs) {
-    ctx.body = setResponse(
-      [],
-      999,
-      `系统监测该用户已存在,信息如下${params.username}-${params.email}`
-    )
+    ctx.body = setResponse([], 999, `系统监测该用户已存在,用户名称${params.username}或用户邮箱${params.email}重复`)
   } else {
     const counrDocs = await Count.findOneAndUpdate(
       { _id: 'userId' },
@@ -83,19 +46,17 @@ router.post('/add', async ctx => {
     })
     ctx.body = setResponse([], 200)
   }
-  // User.create(params)
 })
 
 router.post('/update', async ctx => {
   const params = ctx.request.body
-  console.log(params)
   await User.findOneAndUpdate({ username: params.username }, { ...params })
   ctx.body = setResponse([], 200)
 })
 
 router.get('/del', async ctx => {
   const { userId } = ctx.request.query
-  await User.findOneAndDelete({ userId })
+  await User.findOneAndUpdate({ userId }, { isDelete: 1 })
   ctx.body = setResponse([], 200)
 })
 module.exports = router
